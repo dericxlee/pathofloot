a267d6e7c0c4265727dae967c8ef94e96bdf57f6
dazedgit <103487062+dazedgit@users.noreply.github.com>
2025-04-17 18:41:49 -0700

    connect psql

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express();
 const PORT = 5000
+const pool = require('./db');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello')
+});
+
+app.get('/dbtest', async(req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Database error');
+    }
 });
 
 app.listen(PORT, () => {
